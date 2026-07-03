e9906b887d3cfc62384c429d5509eabaeb9bfd28
mbaroland <mbaroland.kumena@ifnti.com>
2026-07-03 14:13:34 +0000

    API Rest CRUD avec postgresql

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const sequelize = require('./src/db/sequelize')
+const app = express()
+
+const port = 3000
+
+app.use(bodyParser.json())
+
+
+sequelize.initDb()
+
+// 
+require('./src/routes/findAllPokemon')(app)
+
+require('./src/routes/findPokemonByPk')(app)
+
+require('./src/routes/createPokemon')(app)
+
+require('./src/routes/updatePokemon')(app)
+
+require('./src/routes/deletePokemon')(app)
+
+app.listen(port, ()=> console.log(`Votre appli a bien démaré sur le http://localhost:${port}`))
---
+++ src/models/pokemon.js
@@ -0,0 +1,55 @@
+/* Déclaration d'un model */
+const { Sequelize, DataTypes } = require("sequelize");
+
+module.exports = (Sequelize , DataTypes)  => {
+    return Sequelize.define(
+        'Pokemon', 
+        {
+            id : {
+                type : DataTypes.INTEGER,
+                primaryKey : true,
+                autoIncrement : true
+            },
+
+            name : {
+                type : DataTypes.STRING,
+                allowNull : false
+            },
+
+            hp : {
+                type : DataTypes.INTEGER,
+                allowNull : false
+            },
+
+            cp : {
+                type : DataTypes.INTEGER,
+                allowNull : false
+            },
+
+
+            picture : {
+                type : DataTypes.STRING,
+                allowNull : false
+            },
+
+            types : {
+                type : DataTypes.STRING,
+                allowNull : false,
+                get() {
+                    return this.getDataValue('types').split(',')
+                },
+                set(types){
+                    this.setDataValue('types', types.join())
+                }
+            }
+
+        },
+        {
+            timestamps : true,
+            createdAt : 'created', //renommage de createdAt en created
+            updatedAt : true // Date de mis à jour 
+        }
+
+
+    )
+}
